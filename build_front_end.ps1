8f3e38cef90b96648ddce6971363c3b8eb5dabdc
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-11 21:48:45 +0330

    fixed ps script

--- build_front_end.ps1
+++ build_front_end.ps1
@@ -1,5 +1,6 @@
 cd ted-react-version
-BABEL_ENV=production webpack --config "config/webpack.config.dev.js"
+$env:BABEL_ENV = "production"
+webpack --config "config/webpack.config.dev.js"
 Remove-Item –Path "../front_end/static/" -Recurse
 New-Item -ItemType directory -Path "../front_end/static/"
 Copy-Item -Path "dist/*" -Destination "../front_end/static/" -Recurse
