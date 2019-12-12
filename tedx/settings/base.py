04f9c6ae847af5222e979c06f86a065d3bc3e2ad
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-12 20:47:06 +0330

    fixed 403 on registration

--- tedx/settings/base.py
+++ tedx/settings/base.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     )
 }
 
