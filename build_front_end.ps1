b0561ffafd26d43e096d47430dde55242afbe34f
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-13 21:59:24 +0330

    edited build scripts and added database

--- build_front_end.ps1
+++ build_front_end.ps1
@@ -8,3 +8,4 @@ New-Item -ItemType directory -Path "../front_end/static/"
 
 webpack --config "config/webpack.config.dev.js"
 Copy-Item -Path "dist/*" -Destination "../front_end/static/" -Recurse
+Copy-Item -Path "src/assets/images/*" -Destination "../front_end/static/img/src/assets/images" -Recurse
--- build_front_end.sh
+++ build_front_end.sh
@@ -1,9 +1,10 @@
 cd ted-react-version
 
-rm -rf dist/
-rm -rf ../front_end/static/
+rm -rf dist/*
+rm -rf ../front_end/static/*
 
 mkdir -p ../front_end/static/
 
 BABEL_ENV=production webpack --config "config/webpack.config.dev.js"
 cp -rf dist/* ../front_end/static/
+cp -rf src/assets/images/* ../front_end/static/img/src/assets/images
