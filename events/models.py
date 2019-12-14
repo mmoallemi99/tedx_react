aff92b3346a8e088ab635b5c158c8d0282357ee0
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-14 22:21:04 +0330

    fixes

--- events/models.py
+++ events/models.py
@@ -18,8 +18,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Staff(models.Model):
-    # event = models.ForeignKey(Event,
-    #                           on_delete=models.CASCADE, )
     first_name = models.CharField(max_length=30, verbose_name='first name')
     last_name = models.CharField(max_length=30, verbose_name='last name')
 
@@ -50,44 +48,32 @@ class Staff(models.Model):
     role = models.CharField(choices=team_role_choices, max_length=30,
                             help_text='team\'s way of association in team', default='others', null=True, blank=True)
 
-    bio = models.TextField(blank=True, null=True, help_text='description about person e.g. role in team achievements')
     picture = models.ImageField(upload_to='pics/team/', help_text='team\'s picture')
     linkedin_account = models.CharField(max_length=100, null=True, blank=True,
                                         help_text='linkedin.com/in/[User_Account]')
+    instagram_account = models.CharField(max_length=100, null=True, blank=True,
+                                         help_text='instagram.com/[User_Account]')
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
 
 
 class Speaker(models.Model):
-    # event = models.ForeignKey(Event,
-    #                           on_delete=models.CASCADE,
-    #                           help_text='Choose Your Event:', null=True, blank=True)
-
-    STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('deactive', 'Deactive'),
-    ]
-
     first_name = models.CharField(max_length=30, verbose_name='first name')
     last_name = models.CharField(max_length=30, verbose_name='last name')
     picture = models.ImageField(upload_to='pics/speakers/',
                                 default='default.jpg', null=True, blank=True)
-    email = models.EmailField()
-    phone_number = PhoneNumberField(null=True, blank=True)
-    bio = models.TextField(max_length=500)
-    account_status = models.CharField(choices=STATUS_CHOICES,
-                                      default='deactive', max_length=20)
+    topic = models.CharField(max_length=30)
+    linkedin_account = models.CharField(max_length=100, null=True, blank=True,
+                                        help_text='linkedin.com/in/[User_Account]')
+    instagram_account = models.CharField(max_length=100, null=True, blank=True,
+                                         help_text='instagram.com/[User_Account]')
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
 
 
 class Sponsor(models.Model):
-    # event = models.ForeignKey(Event,
-    #                           on_delete=models.CASCADE,
-    #                           help_text='Choose Your Event:', )
-
     sponsor_type_choices = [('financial', 'Financial'),
                             ('spiritual', 'Spiritual'),
                             ('other', 'Other'), ]
@@ -127,3 +113,6 @@ class Attendee(models.Model):
     what_made_you = models.TextField(max_length=400)
     how_familiar = models.TextField(max_length=400)
     expects = models.TextField(max_length=400)
+
+    def __str__(self):
+        return self.full_name
