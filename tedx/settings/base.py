df4f3d8128e49464f13a3994c0b7caaef5e28c21
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-14 21:34:51 +0330

    changed media directory and fixed frontend team

--- tedx/settings/base.py
+++ tedx/settings/base.py
@@ -129,7 +129,7 @@ STATICFILES_DIR = (
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
