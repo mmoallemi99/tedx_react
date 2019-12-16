bc2e9d305b79e292141b7003aeb570921884f642
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-16 21:11:57 +0330

    fixed dev db

--- tedx/settings/dev.py
+++ tedx/settings/dev.py
@@ -4,23 +4,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tedx',
-        'USER': 'tedx',
-        'PASSWORD': '4EunuP6D/cD6XWhYERE=',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tedx',
+#         'USER': 'tedx',
+#         'PASSWORD': '4EunuP6D/cD6XWhYERE=',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 INSTALLED_APPS += [
     'corsheaders',
